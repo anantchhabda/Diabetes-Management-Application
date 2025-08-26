f4f4e01c2bc1af861895f2ca5a9bc6ea857ab552
Anant Chhabda <anantchhabda@Anants-MacBook-Air.local>
2025-08-26 20:34:47 +0800

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    // For PWA support, if you plan to use next-pwa plugin
+    modern: true
+  }
+};
 
 export default nextConfig;
