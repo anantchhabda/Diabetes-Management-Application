49dad33f2ecb260ddd84020ff55a49cd19248728
Khanh Do <qbkhanhdo0106@gmail.com>
2025-09-03 15:30:11 +0800

    Add register and login api

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,10 +1,22 @@
-export const runtime = 'nodejs';
-export const dynamic = 'force-dynamic';
-import dbConnect from '@/lib/db';
-import User from '@/lib/models/User';
-import bcrypt from 'bcryptjs';
+import dbConnect from '../../../../lib/db';
+import User from '../../../../lib/models/User';
 import {NextResponse} from 'next/server';
+
 export async function POST(req) {
-    await dbConnect();
-    const {phoneNumber, password, role} = await req.json();
+    await dbConnect(); //connect to MongoDB
+    const {phoneNumber, password, role} = await req.json(); //read user data
+    const existUser = await User.findOne({phoneNumber});
+    if (existUser) {
+        return NextResponse.json(
+            {message: 'User already exists'},
+            {status: 400} //bad request
+        );
+    }
+    const user = await User.create({phoneNumber, password, role}); //save uder data
+    return NextResponse.json(
+        {_id: user._id,
+        phoneNumber: user.phoneNumber,
+        role: user.role},
+        {status: 201} //created
+    );
 }
