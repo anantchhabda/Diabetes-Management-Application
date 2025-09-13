c83cd492791eb6e06a8953a7c4b441c37031d882
Janet Nguyen <171430048+nhuntkn@users.noreply.github.com>
2025-09-14 05:02:00 +0800

    Update route.js

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,13 +1,18 @@
-import dbConnect from '../../../../lib/db';
-import User from '../../../../lib/models/User';
+import dbConnect from '../../../lib/db';
+import mongoose from 'mongoose';
+import User from '../../../lib/models/User';
 import {NextResponse} from 'next/server';
-import {signJwt} from '../../../../lib/auth';
+import {signJwt} from '../../../lib/auth';
 
 export async function POST(req) {
     await dbConnect(); //connect to MongoDB
     const {phoneNumber, password, role} = await req.json(); //read user data
+    
     if (!phoneNumber || !password  || !role) {
-        return NextResponse.json({error: "phone, password, confirmPassword, role are required"}, {status: 400});
+        return NextResponse.json(
+            {error: "phone, password, confirmPassword, role are required"}, 
+            {status: 400}
+        );
     }
 
     const existUser = await User.findOne({phoneNumber});
@@ -17,11 +22,18 @@ export async function POST(req) {
             {status: 400} //bad request
         );
     }
+
     try {
         const user = await User.create({phoneNumber, password, role}); //save user data
-        const token = signJwt({_id: user._id, phoneNumber: user.phoneNumber, role: user.role, scope: 'onboarding'});
+        const token = signJwt({
+            sub: user._id, 
+            phoneNumber: user.phoneNumber, 
+            role: user.role, 
+            scope: 'onboarding'
+        });
+
         return NextResponse.json(
-            {_id: user._id,
+            {userId: user._id,
             phoneNumber: user.phoneNumber,
             role: user.role,
             token},
