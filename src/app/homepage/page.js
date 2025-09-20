a93c4c1be84a3dd11461984bfbed836d2b7d1839
aradavi63 <adam.ahmadavi@gmail.com>
2025-09-20 20:34:35 +0800

    change css to use ones listed in global css

--- src/app/homepage/page.js
+++ src/app/homepage/page.js
@@ -6,28 +6,26 @@ export default function HomePage() {
   const userName = "John Doe"; // can be dynamic later
 
   return (
-    <div>
+    <div className="min-h-screen bg-[var(--color-background)]">
       <Header />
-      <main className="flex flex-col justify-center items-center min-h-screen px-4 gap-8 bg-[rgba(58,211,249,1)]">
+      <main className="flex flex-col justify-center items-center px-4 gap-8 pt-8">
         <button
           type="button"
-          className="w-full max-w-xs py-3 bg-[#00C896] text-white text-lg rounded-md text-center hover:opacity-90 transition"
+          className="w-full max-w-xs py-3 bg-[var(--color-tertiary)] text-[var(--color-textWhite)] text-lg rounded-md text-center hover:opacity-90 transition"
         >
           Hello {userName}
         </button>
 
-        {/* Button to log data --> Glucose/Insulin*/}
         <button
           type="button"
-          className="w-full max-w-xs py-3 bg-[#004B5E] text-white text-lg rounded-md text-center hover:opacity-90 transition"
+          className="w-full max-w-xs py-3 bg-[var(--color-secondary)] text-[var(--color-textWhite)] text-lg rounded-md text-center hover:opacity-90 transition"
         >
           Log Data
         </button>
 
-        {/* Set customer reminders */}
         <button
           type="button"
-          className="w-full max-w-xs py-3 bg-[#00C896] text-white text-lg rounded-md text-center hover:opacity-90 transition"
+          className="w-full max-w-xs py-3 bg-[var(--color-tertiary)] text-[var(--color-textWhite)] text-lg rounded-md text-center hover:opacity-90 transition"
         >
           Set Reminders
         </button>
