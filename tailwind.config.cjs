41981a9d26f9b32aad067e13613878cc5041b2b2
Anant Chhabda <anantchhabda@Anants-iMac.local>
2025-10-05 17:34:44 +0800

    removed tailwind v4

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./src/components/**/*.{js,jsx,ts,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx,mdx}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
