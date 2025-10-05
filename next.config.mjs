9b667fc903341629c5e6b26340a5869351d578bc
Anant Chhabda <anantchhabda@Anants-iMac.local>
2025-10-05 17:00:47 +0800

    disable lightningCSS globally for vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Disable LightningCSS (it breaks Vercel Linux builds)
-  compiler: {
-    lightningcss: false,
-  },
   experimental: {
     optimizeCss: false,
   },
